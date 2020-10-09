9d9bf770a514cc05f7253ef468a9e9d5488deb0b
Erik Lelis <erikslelis@hotmail.com>
2020-10-08 23:09:03 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/eriklelis/cursomc/services/CategoriaService.java
+++ src/main/java/com/eriklelis/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.eriklelis.cursomc.domain.Categoria;
 import com.eriklelis.cursomc.repositories.CategoriaRepository;
+import com.eriklelis.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,7 +15,8 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	} 
 }
