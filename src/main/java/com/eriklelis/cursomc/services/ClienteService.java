b91ee64f31acbb431276373a0ec9376cf1da653b
Erik Lelis <erikslelis@hotmail.com>
2021-02-03 15:01:17 -0300

    Endpoint cliente/id disponivel

---
+++ src/main/java/com/eriklelis/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.eriklelis.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eriklelis.cursomc.domain.Cliente;
+import com.eriklelis.cursomc.repositories.ClienteRepository;
+import com.eriklelis.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		 Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	} 
+}
