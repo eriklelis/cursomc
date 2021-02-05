3c37dca2a1498034779c0bd83f2a65e58132c586
Erik Lelis <erikslelis@hotmail.com>
2021-02-05 00:46:35 -0300

    Inserindo novo cliente com POST

--- src/main/java/com/eriklelis/cursomc/resources/ClienteResource.java
+++ src/main/java/com/eriklelis/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.eriklelis.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eriklelis.cursomc.domain.Cliente;
 import com.eriklelis.cursomc.dto.ClienteDTO;
+import com.eriklelis.cursomc.dto.ClienteNewDTO;
 import com.eriklelis.cursomc.services.ClienteService;
 
 @RestController
@@ -66,6 +69,15 @@ public class ClienteResource {
 		Page<Cliente> list = service.findPage(page,linesPerPage,orderBy,direction);
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDto(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/eriklelis/cursomc/services/ClienteService.java
+++ src/main/java/com/eriklelis/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.eriklelis.cursomc.domain.Cidade;
 import com.eriklelis.cursomc.domain.Cliente;
+import com.eriklelis.cursomc.domain.Endereco;
+import com.eriklelis.cursomc.domain.enums.TipoCliente;
 import com.eriklelis.cursomc.dto.ClienteDTO;
+import com.eriklelis.cursomc.dto.ClienteNewDTO;
 import com.eriklelis.cursomc.repositories.ClienteRepository;
+import com.eriklelis.cursomc.repositories.EnderecoRepository;
 import com.eriklelis.cursomc.services.exceptions.DataIntegrityException;
 import com.eriklelis.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -20,6 +26,9 @@ import com.eriklelis.cursomc.services.exceptions.ObjectNotFoundException;
 public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
 		 Optional<Cliente> obj = repo.findById(id);
@@ -58,5 +67,28 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 		
+	}
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+	
+	public Cliente fromDto(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null,objDto.getNome(), objDto.getEmail(),objDto.getCpfOuCnpj(),TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(),null,null);
+		Endereco end = new Endereco(null,objDto.getLogradouro(),objDto.getNumero(),objDto.getComplemento(),objDto.getBairro(),objDto.getCep(),cli,cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if(objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 }
