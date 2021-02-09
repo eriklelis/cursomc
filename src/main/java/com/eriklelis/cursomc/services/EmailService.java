c16913d7ee72078a93462d7cc79d6f69fd98a734
Erik Lelis <erikslelis@hotmail.com>
2021-02-09 00:29:42 -0300

    Email Html

--- src/main/java/com/eriklelis/cursomc/services/EmailService.java
+++ src/main/java/com/eriklelis/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.eriklelis.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.eriklelis.cursomc.domain.Pedido;
@@ -7,6 +9,10 @@ import com.eriklelis.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-
 	void sendEmail(SimpleMailMessage msg);
+	
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/eriklelis/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/eriklelis/cursomc/services/SmtpEmailService.java
@@ -1,21 +1,35 @@
 package com.eriklelis.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	JavaMailSender javaMailSender;
+
+	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		
 		LOG.info("Enviando de email...");
 		mailSender.send(msg);
+		LOG.info("Email enviado");
+	}
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando de email...");
+		javaMailSender.send(msg);
 		LOG.info("Email enviado");
 	}
 	
